9b7fed2af421e4a35f7d604c8d698cbebd19509d
Adel Aaji <adel.aaji@gmail.com>
2021-05-31 23:54:13 +0300

    Move files out level to be able to publish.

--- photos-grid/api/unsplash/index.js
+++
@@ -1,9 +0,0 @@
-import { UNSPLASH_ACCESS_KEY } from "../../config";
-import { createApi } from "unsplash-js";
-const unsplash = createApi({
-  accessKey: UNSPLASH_ACCESS_KEY,
-});
-
-export const getPhotos = (page = 1, perPage = 50) => {
-  return unsplash.photos.list({ page, perPage });
-};
